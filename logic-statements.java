db9bde9989b81a195d3900440d627b5fd23fbb48
BrainiacMainiac <BrainiacMainiac@users.noreply.github.com>
2015-05-07 09:39:01 -0400

    Got a clue.

--- logic-statements.java
+++ logic-statements.java
@@ -3,20 +3,20 @@ class LogicStatements {
     // and statements
     for(int i = 1; i > 4; i++){
       if(i = 1){
-        bool 1 = true;
-        bool 2 = false;
+        boolean 1 = true;
+        boolean 2 = false;
         return 1 && 2;
       } else if(i = 2) {
-        bool 1 = true;
-        bool 2 = false;
+        boolean 1 = true;
+        boolean 2 = false;
         return 1 && 2;
       } else if(i = 3){
-        bool 1 = false;
-        bool 2 = true;
+        boolean 1 = false;
+        boolean 2 = true;
         return 1 && 2;
       } else {
-        bool 1 = false;
-        bool 2 = false;
+        boolean 1 = false;
+        boolean 2 = false;
         return 1 && 2;
       }
     }

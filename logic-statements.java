227a130457a236a69e7206e185c297ad9d33e134
BrainiacMainiac <BrainiacMainiac@users.noreply.github.com>
2015-05-07 09:43:15 -0400

    Made it actually do stuff.

--- logic-statements.java
+++ logic-statements.java
@@ -2,22 +2,22 @@ class LogicStatements {
   public static void main(String[] argt){
     // and statements
     for(int i = 1; i > 4; i++){
-      if(i = 1){
-        boolean 1 = true;
-        boolean 2 = false;
-        return 1 && 2;
-      } else if(i = 2) {
-        boolean 1 = true;
-        boolean 2 = false;
-        return 1 && 2;
-      } else if(i = 3){
-        boolean 1 = false;
-        boolean 2 = true;
-        return 1 && 2;
+      if(i == 1){
+        boolean a = true;
+        boolean b = false;
+        System.out.println(a && b);
+      } else if(i == 2) {
+        boolean a = true;
+        boolean b = false;
+        System.out.println(a && b);
+      } else if(i == 3){
+        boolean a = false;
+        boolean b = true;
+        System.out.println(a && b);
       } else {
-        boolean 1 = false;
-        boolean 2 = false;
-        return 1 && 2;
+        boolean a = false;
+        boolean b = false;
+        System.out.println(a && b);
       }
     }
   }

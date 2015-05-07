2694501018bc2b1bbf0b1741232b2288a9ee6224
BrainiacMainiac <BrainiacMainiac@users.noreply.github.com>
2015-05-07 09:35:57 -0400

    Initialized cod.

---
+++ logic-statements.java
@@ -0,0 +1,24 @@
+class LogicStatements {
+  public static void main(String[] argt){
+    // and statements
+    for(int i = 1; i > 4; i++){
+      if(i = 1){
+        bool 1 = true;
+        bool 2 = false;
+        return 1 && 2;
+      } else if(i = 2) {
+        bool 1 = true;
+        bool 2 = false;
+        return 1 && 2;
+      } else if(i = 3){
+        bool 1 = false;
+        bool 2 = true;
+        return 1 && 2;
+      } else {
+        bool 1 = false;
+        bool 2 = false;
+        return 1 && 2;
+      }
+    }
+  }
+}

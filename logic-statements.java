73cdaee6afe2490eb20476ebee899904c8c872c6
BrainiacMainiac <BrainiacMainiac@users.noreply.github.com>
2015-05-07 09:47:45 -0400

    Added or statements.

--- logic-statements.java
+++ logic-statements.java
@@ -1,6 +1,6 @@
 class LogicStatements {
   public static void main(String[] argt){
-    // and statements
+    System.out.println("And statements:");
     for(int i = 1; i < 5; i++){
       if(i == 1){
         boolean a = true;
@@ -18,6 +18,26 @@ class LogicStatements {
         boolean a = false;
         boolean b = false;
         System.out.println(a && b);
+      }
+    };
+    System.out.println("Or statements");
+    for(int i = 1; i < 5; i++){
+      if(i == 1){
+        boolean a = true;
+        boolean b = true;
+        System.out.println(a || b);
+      } else if(i == 2) {
+        boolean a = true;
+        boolean b = false;
+        System.out.println(a || b);
+      } else if(i == 3){
+        boolean a = false;
+        boolean b = true;
+        System.out.println(a || b);
+      } else {
+        boolean a = false;
+        boolean b = false;
+        System.out.println(a || b);
       }
     }
   }

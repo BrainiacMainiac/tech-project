345e6aeb546f222bff723b6273d9fcf39289becb
joeshmoe999 <bobthequark@gmail.com>
2015-05-08 15:08:58 -0400

    added more stuff

--- main-code.java
+++ main-code.java
@@ -1,7 +1,19 @@
 // Import javax.swing.*
 import javax.swing.*;
+import java.awt.event.*;
+import java.awt.*;
 class TechProject extends JFrame{
+  MazePanel maze;
+  JButton mazebutton=new JButton("Generate a new maze");
+  JButton solvebutton=new JButton("Solve the Maze");
   public static void main(String[] args){
-    
+    TechProject proj= new TechProject();
+  }
+  public TechProject() {
+    super("Maze Generator/Solver");
+    setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    JPanel bigpanel=new JPanel();
+    BoxLayout lay=new BoxLayout(bigpanel,BoxLayout.X_AXIS);
+    bigpanel.setLayout(lay);
   }
 }

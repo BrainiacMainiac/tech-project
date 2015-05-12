ce987f2d5a364e98ff41e94bb0d3692ed7f67a04
joeshmoe999 <bobthequark@gmail.com>
2015-05-12 14:25:29 -0400

    debugged it

--- main-code.java
+++ main-code.java
@@ -2,15 +2,16 @@
 import javax.swing.*;
 import java.awt.event.*;
 import java.awt.*;
-class TechProject extends JFrame implements ActionListener{
+class TechProject extends JFrame// implements ActionListener
+{
   //Instance variables/objects which all methods must be able to see
   MazePanel maze=new MazePanel();
   JButton mazebutton=new JButton("Generate a new maze");
   JButton solvebutton=new JButton("Solve the Maze");
   JSlider width=new JSlider(2,100,10);
-  JSlider heigth=new JSlider(2,100,10);
-  JComboBox generatemethod=new JComboBox();
-  JComboBox solvemethod=new JComboBox();
+  JSlider height=new JSlider(2,100,10);
+  JComboBox generatemethod;
+  JComboBox solvemethod;
   public static void main(String[] args){
     TechProject proj= new TechProject();
   }
@@ -23,17 +24,20 @@ class TechProject extends JFrame implements ActionListener{
     bigpanel.setLayout(lay);
     width.setPaintLabels(true);
     height.setPaintLabels(true);
-    generatemethod.add({"Depth-First", "Prim Algorithm", "Recursive Division"});
-    solvemethod.add({"Tramaux Algorithm", "A# Search", "Dead end filling"});
+    String[] generates={"Depth-First", "Prim Algorithm", "Recursive Division"};
+    String[] solves={"Tramaux Algorithm", "A# Search", "Dead end filling"};
+    generatemethod=new JComboBox(generates);
+    solvemethod=new JComboBox(solves);
     JPanel bottom=new JPanel();
     bottom.add(width);
-    bottom.add(heigth);
+    bottom.add(height);
     bottom.add(generatemethod);
     bottom.add(solvemethod);
     bottom.add(mazebutton);
     bottom.add(solvebutton);
     bigpanel.add(maze);
     bigpanel.add(bottom);
+    add(bigpanel);
     setVisible(true);
   }
 }

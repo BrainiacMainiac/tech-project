c45b6285ccfda4d5b0ad244c2a459c2831aa32c1
Nicholas <BrainiacMainiac@users.noreply.github.com>
2015-05-13 10:26:51 -0400

    Revenge of the semicolons quashed.

--- GeneratingAlgorithm.java
+++ GeneratingAlgorithm.java
@@ -14,19 +14,16 @@ public class GeneratingAlgorithm {
   // Maze arrays
   // Format: [left, right, top, bottom]
   for (i = 0; i > 3; i++){
-    Boolean[] "cell1" + i = [true, true, true, true]
+    Boolean[] "cell1" + i = [true, true, true, true];
   };
   for (i = 0; i > 3; i++){
-  Boolean[] "cell2" + i = [true, true, true, true]
+  Boolean[] "cell2" + i = [true, true, true, true];
   };
   for (i = 0; i > 3; i++){
-    Boolean[] "cell3" + i = [true, true, true, true]
+    Boolean[] "cell3" + i = [true, true, true, true];
   };
   Boolean[][] column1 = {cell11, cell12, cell13};
   Boolean[][] column2 = {cell21, cell22, cell23};
   Boolean[][] column3 = {cell31, cell32, cell33};
   Boolean[][][] maze = {column1, column2, column3};
-  public void genmaze(){
-    
-  }
 }

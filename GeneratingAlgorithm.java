75947242ae6b5817eae0b616c641065a65336dcc
Nicholas <BrainiacMainiac@users.noreply.github.com>
2015-05-13 08:19:37 -0400

    Put in key/

--- GeneratingAlgorithm.java
+++ GeneratingAlgorithm.java
@@ -1,1 +1,15 @@
 // Uses randomized Prim's algorithm
+/* 
+.=open
+/=wall
++=start
+-=end
+*=rigth path
+:=wrong path
+x=dead end filled
+f[int]=wrong A#
+t[int]=right/possible A#
+*/
+public class GeneratingAlgorithm {
+  String[] column1 = {
+}

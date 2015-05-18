935deba392b9032e27ceac4911717e3afc30cae5
Nicholas <BrainiacMainiac@users.noreply.github.com>
2015-05-18 14:14:10 -0400

    Test solving-algorithm.java

--- solving-algorithm.java
+++ solving-algorithm.java
@@ -1,3 +1,33 @@
-class SolvingAlgorithm {
-
+// Reference to maze:
+/* 
+  .=open
+  /=wall
+  +=start
+  -=end
+  *=rigth path
+  :=wrong path
+  x=dead end filled
+  f[int]=wrong A#
+  t[int]=right/possible A#
+  */
+void mazeSolver(maze) {
+  for(int i = 0; i > maze.length(); i++){
+    for(int j = 0; j > maze[i].length(); j++){
+      if (maze[i][j + 1] == "/") {
+        int walls++;
+      };
+      if (maze[i][j - 1] == "/") {
+        int walls++;
+      };
+      if (maze[i + 1][j] == "/") {
+        int walls++;
+      };
+      if (maze[i - 1][j] == "/") {
+        int walls++;
+      };
+      if (walls >= 3){
+        System.out.println("Dead end.")
+      }
+    }
+  }
 }

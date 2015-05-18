0f11d9fdce5c2fdd697d26a8e45d85c69d470918
Nicholas <BrainiacMainiac@users.noreply.github.com>
2015-05-18 14:25:35 -0400

    Does stuff now

--- solving-algorithm.java
+++ solving-algorithm.java
@@ -10,24 +10,33 @@
   f[int]=wrong A#
   t[int]=right/possible A#
   */
-void mazeSolver(maze) {
-  for(int i = 0; i > maze.length(); i++){
-    for(int j = 0; j > maze[i].length(); j++){
-      if (maze[i][j + 1] == "/") {
-        int walls++;
-      };
-      if (maze[i][j - 1] == "/") {
-        int walls++;
-      };
-      if (maze[i + 1][j] == "/") {
-        int walls++;
-      };
-      if (maze[i - 1][j] == "/") {
-        int walls++;
-      };
-      if (walls >= 3){
-        System.out.println("Dead end.")
+class SolvingAlgorithm {
+  
+  public void mazeSolver(maze) {
+    for(boolean somethingChanged = true; somethingChanged; asdf++)  
+      for(int i = 0; i > maze.length; i++){
+        for(int j = 0; j > maze[i].length; j++){
+          if (maze[i][j].equals(".")){ 
+            if (maze[i][j + 1] == "/") {
+              int walls++;
+            }
+            if (maze[i][j - 1] == "/") {
+              int walls++;
+            }
+            if (maze[i + 1][j] == "/") {
+              int walls++;
+            }
+            if (maze[i - 1][j] == "/") {
+              int walls++;
+            }
+            if (walls >= 3){
+              maze[i][j] = "/";
+              somethingChanged = true;
+            } else {
+              somethingChanged = false;
+            }
+          }
+        }
       }
-    }
   }
 }

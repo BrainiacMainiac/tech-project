f8915c8f86b5a2e4225a49e215e51d2f24fb347c
Nicholas <BrainiacMainiac@users.noreply.github.com>
2015-05-21 11:14:21 -0400

    Added comments

--- solving-algorithms.java
+++ solving-algorithms.java
@@ -11,6 +11,7 @@
   t[int]=right/possible A#
   */
 class DeadEndBlockerAlgorithm {
+  // Test maze please delete
   static String[][] maze1 = {
     {"/","/","/","/","/","/","/"},
     {"/","+","/",".",".",".","/"},
@@ -21,11 +22,17 @@ class DeadEndBlockerAlgorithm {
     {"/","/","/","/","/","/","/"}
   };
   static void deadEndSolve(String mazeArray[][]) {
+    // Make sure something changes every time
     for(boolean somethingChanged = true; somethingChanged; somethingChanged = false){
+      // Outer loop for x
       for(int i = 0; i > mazeArray.length; i++){
+        // Inner loop for y
         for(int j = 0; j > mazeArray[i].length; j++){
+          // Make sure you're marking an open spot
           if (mazeArray[i][j].equals(".")){ 
+            // Surrounding walls check
             int walls = 0;
+            // Note: / = wall, x = filled dead end
             if (mazeArray[i][j + 1] == "/" || mazeArray[i - 1][j] == "x") {
               walls++;
             }
@@ -38,14 +45,14 @@ class DeadEndBlockerAlgorithm {
             if (mazeArray[i - 1][j] == "/" || mazeArray[i - 1][j] == "x") {
               walls++;
             }
+            // If surrounded by 3 or more filled dead ends or walls
             if (walls >= 3){
+              // Fill in space
               mazeArray[i][j] = "x";
+              // Mention something was changed
               somethingChanged = true;
+              // Update the window
               repaint();
-              try {
-                Thread.sleep(100);
-              } catch (Exception e) {
-              }
               }
             }
           }
@@ -54,6 +61,7 @@ class DeadEndBlockerAlgorithm {
     }
   }
   public static void main(String[] argt){
+    // Run a test
     mazeSolver(maze1);
       }
     }

8bb0d15981337a64b62f3064a1e7d92566931398
joeshmoe999 <bobthequark@gmail.com>
2015-05-27 15:16:02 -0400

    Update main-code.java

--- main-code.java
+++ main-code.java
@@ -29,6 +29,7 @@ class TechProject extends JFrame implements ActionListener
       maze.mode=MazePanel.GENDIV;
     }
     if (sor==solvebutton) {
+      if ()
       maze.mode=MazePanel.DEAD_END;
     }
     maze.current=new Thread(maze);
@@ -81,6 +82,7 @@ class MazePanel extends JPanel implements Runnable{
   Thread current;
   final static byte GENDIV=0;
   final static byte DEAD_END=1;
+  final static byte DEPTH_FIRST=2;
   byte mode;
   /* 
   .=open
@@ -101,6 +103,11 @@ class MazePanel extends JPanel implements Runnable{
     }
     if (mode==DEAD_END) {
       deadEndSolve();
+    }
+    if (mode=DEPTH_SEARCH) {
+      int wid=TechProject.proj.width.getValue()*2+1;
+      int hei=TechProject.proj.height.getValue()*2+1;
+      generateDFS(hei,wid);
     }
   }
   public MazePanel() {
@@ -334,4 +341,55 @@ public String[][] generate(int rows, int cols) {
         }
       }
   }
+  public void generateDFS(int rows, int cols) {
+    mazeArray = new String[rows][cols];
+    for(int i = 0; i < mazeArray.length; i++){
+        for(int j = 0; j < mazeArray[i].length; j++){
+            mazeArray[i][j] = "/";
+        }
+    }
+    DFS(1,1);
+    mazeArray[1][1]="+";
+    mazeArray[rows-2][cols-2]="-";
+}
+public void DFS(int row, int col) {
+    ArrayList li = new ArrayList();
+    li.add("left");
+    li.add("right");
+    li.add("up");
+    li.add("down");
+    Collections.shuffle(li);
+    for (int i=0; i<4; i++) {
+    String test = li.get(i);
+    if (first.equals("left")) {
+        if (col!=1 && mazeArray[row][col].equals("/")) {
+            mazeArray[row][col-2]=".";
+            mazeArray[row][col-1]=".";
+            DFS(row,col-2);
+        }
+    }
+    if (first.equals("right")) {
+        if (col!=mazeArray[0].length-2 && mazeArray[row][col].equals("/")) {
+            mazeArray[row][col+2]=".";
+            mazeArray[row][col+1]=".";
+            DFS(row,col+2);
+        }
+    }
+    if (first.equals("up")) {
+        if (row!=1 && mazeArray[row][col].equals("/")) {
+            mazeArray[row-2][col]=".";
+            mazeArray[row-1][col]=".";
+            DFS(row-2,col);
+        }
+    }
+    if (first.equals("down")) {
+        if (row!=mazeArray.length-2 && mazeArray[row][col].equals("/")) {
+            mazeArray[row+2][col]=".";
+            mazeArray[row+1][col]=".";
+            DFS(row+2,col);
+        }
+    }
+}
+}
+    
 }

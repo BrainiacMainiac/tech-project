511cdd4610b577e54c49074d73846d73538d394c
joeshmoe999 <bobthequark@gmail.com>
2015-06-02 14:37:15 -0400

    Update prim.java

--- prim.java
+++ prim.java
@@ -1,1 +1,22 @@
+public void genPrim(int rows, int cols) {
+  mazeArray=new String[rows][cols];
+  for (int i=0; i<rows; i++) {
+    for (int j=0; j<cols; j++) {
+      mazeArray[i][j]="/";
+    }
+  }
+  ArrayList walls=new ArrayList(60);
+  while (walls.size()>0) {
+    int n=(int)Math.floor(Math.random()*walls.size());
+    
+  }
+}
 
+
+class PrimWall {
+  int row=0;
+  int col=0;
+  public static final int UP=0;
+  public static final int UP=0;
+  public static final int UP=0;
+}
